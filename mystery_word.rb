0b7d01d407558500cdc28c0e23fdc2c118912785
Brandon Fitch <fitchbj@gmail.com>
2016-09-16 15:38:50 -0400

    It works. Will take your word and save appropriate words in its own array.

--- mystery_word.rb
+++ mystery_word.rb
@@ -1,11 +1,26 @@
-def main()
-  read_full_pool()
 
+def length_and_sort(level_choice, word_pool)
+  if level_choice == "EASY"
+    word_pool.keep_if { |word| (word.length >= 4) && (word.length <= 6) }
+  elsif level_choice == "NORMAL"
+    word_pool.keep_if { |word| (word.length >= 6) && (word.length <= 8) }
+  elsif level_choice == "HARD"
+    word_pool.keep_if { |word| word.length > 8 }
+  else
+    puts "You can't do that."
+  end
+end
+
+def main()
+  word_pool = File.new("/usr/share/dict/words", 'r')
+  word_pool = word_pool.readlines.map(&:chomp)
   puts "What level would you like to play? Type Easy, Normal, or Hard."
   print "> "
   level_choice = gets.chomp.upcase
-
-  error_message = "You can't do that."
+  length_and_sort(level_choice, word_pool)
+  #function that randomizes goes here
+  #letter and submission phase and functions
+  #loop to play again
 
 end
 
@@ -13,54 +28,31 @@ if __FILE__ == $PROGRAM_NAME
   main
 end
 
-word_pool {
-  "EASY" => {
-      1 => "word",
-  },
-  "NORMAL" => {
-      1 => "word",
-  },
-  "HARD" => {
-    1 => "word",
-  }
-}
 
-def read_full_pool(word_pool)
-  word_pool = File.new("/usr/share/dict/words", 'r')
-  word_pool = word_pool.readlines.map(&:chomp)
-end
+# def choose_random()
+#   # easy_words = word_pool["EASY"]
+#
+# end
 
-def length_and_sort(level_choice, word_pool)
-  if level_choice == "EASY"
-    word_pool.keep_if { |word| (word.length >= 4) && (word.length <=6) }
-# PROBLEM: Array of all words as items, within array.
-  elsif level_choice == "NORMAL"
-  elsif level_choice == "HARD"
-  else
-    return error_message
-  end
-end
+# word_pool {
+#   "EASY" => {
+#       1 => "word",
+#   },
+#   "NORMAL" => {
+#       1 => "word",
+#   },
+#   "HARD" => {
+#     1 => "word",
+#   }
+# }
 
-word_pool.length == 4 # || word_pool.length == 5 || word_pool.length == 6
-  # right_words_easy = Hash.new()
-
-
-
-def word_array_matches_level(level_choice, word_pool)
-  if word_pool.has_key?(level_choice)
-    # create storage thing containing possible words to choose random from
-    # pick random word
-    # store random word as variable
-end
-
-def choose_random()
-  easy_words = word_pool["EASY"]
-
-end
-
-
-
-
+# def word_array_matches_level(level_choice, word_pool)
+#   if word_pool.has_key?(level_choice)
+#     # create storage thing containing possible words to choose random from
+#     # pick random word
+#     # store random word as variable
+#   end
+# end
 
 
 
