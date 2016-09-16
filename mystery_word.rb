3ab0de5f5967ecd1c5fddaf90e60d360179f6e14
Brandon Fitch <fitchbj@gmail.com>
2016-09-16 14:31:57 -0400

    Fitch driving.  Added a few functions. Defining and beginning to populate word_pool

--- mystery_word.rb
+++ mystery_word.rb
@@ -1,4 +1,16 @@
+def main()
+  read_full_pool()
 
+  puts "What level would you like to play? Type Easy, Normal, or Hard."
+  print "> "
+  level = gets.chomp.upcase
+
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  main
+end
 
 word_pool {
   "EASY" => [*4..6] => {
@@ -12,16 +24,23 @@ word_pool {
   }
 }
 
-
-def main()
-  word_repo = File.new("/usr/share/dict/words", 'r')
-  word_repo.readlines
-
-  puts "What level would you like to play? Type Easy, Normal, or Hard."
-  print "> "
-  level = gets.chomp.upcase
+def read_full_pool(word_pool)
+  word_pool = File.new("/usr/share/dict/words", 'r')
+  word_pool.readlines.map(&:chomp)
 end
 
-if __FILE__ == $PROGRAM_NAME
-  main
+def length_and_sort(word_pool)
+  right_words = 
+end
+
+def word_array_matches_level(level_choice, word_pool)
+  if word_pool.has_key?(level_choice)
+    # create storage thing containing possible words to choose random from
+    # pick random word
+    # store random word as variable
+end
+
+def choose_random()
+  easy_words = word_pool["EASY"]
+
 end
