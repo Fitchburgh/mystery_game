55de6893eaa6d09e1e50c37e3fcd438b0f2434fa
Brandon Fitch <fitchbj@gmail.com>
2016-09-16 14:04:40 -0400

    Getting play level, started working nested hash of easy, normal, hard words

--- mystery_word.rb
+++ mystery_word.rb
@@ -1,9 +1,25 @@
-File.open("/usr/share/dict/words", 'r') do |words|
 
-end
+
+word_pool {
+  "EASY" => [*4..6] => {
+      1 => "word",
+  },
+  "NORMAL" => [*6..8] => {
+      1 => "word",
+  },
+  "HARD" => [*8..24] => {
+    1 => "word",
+  }
+}
+
 
 def main()
-  #todo write my program
+  word_repo = File.new("/usr/share/dict/words", 'r')
+  word_repo.readlines
+
+  puts "What level would you like to play? Type Easy, Normal, or Hard."
+  print "> "
+  level = gets.chomp.upcase
 end
 
 if __FILE__ == $PROGRAM_NAME
