a48c98495461c62c7e9e87ced821d0445b08f1e7
Brandon Fitch <fitchbj@gmail.com>
2016-09-17 13:07:09 -0400

    Can present random word dependent upon chosen level. if you enter a letter, gets infinite loop on 'not in word'

--- mystery_word.rb
+++ mystery_word.rb
@@ -6,8 +6,25 @@ def length_and_sort(level_choice, word_pool)
     word_pool.keep_if { |word| (word.length >= 6) && (word.length <= 8) }
   elsif level_choice == "hard"
     word_pool.keep_if { |word| word.length > 8 }
+  else
+    puts "NOPE"
   end
 end
+
+def find_and_replace(guess_count, guessed_letter, random_word)
+  loop do |letter|
+    if random_word.include?(guessed_letter)
+      index_point = random_word.index
+      random_word.replace("!")
+    else
+      random_word.include?(guessed_letter)
+      puts "You've guessed wrong!"
+      guess_count += 1
+    end
+  end
+end
+
+# random_word = ["a", "a", "b"]
 
 def find_random_word(right_words)
   random_word = right_words[rand(right_words.length)]
@@ -18,31 +35,10 @@ def find_random_word(right_words)
   puts "#{word_as_blanks}"
 end
 
-def find_and_replace(guess_count, random_word, guessed_letter)
-  loop do |letter|
-  puts "What letter would you like to guess?"
-  print "> "
-  guessed_letter = gets.chomp.downcase
-    while guess_count < 9
-      # go back to loop to check letters + need to get another letter
-      if random_word.include? (guessed_letter)
-        puts "phase one"
-        index_point = random_word.index(guessed_letter)
-        random_word.delete_at(index_point)
-      else
-        guess_count += 1 #only increase when guess is wrong
-        puts "You've guessed wrong! Minus one guess."
-      end
-    end
-  end
-end
-
-# random_word = ["a", "a", "b"]
-
 def main()
-  guess_count = 0
   random_word = []
-  guessed_letter = "a"
+  guess_count = 0
+  guessed_letter = ""
   word_pool = File.new("/usr/share/dict/words", 'r')
   word_pool = word_pool.readlines.map(&:chomp)
   puts "What level would you like to play? Type Easy, Normal, or Hard."
@@ -50,10 +46,14 @@ def main()
   level_choice = gets.chomp.downcase
   if length_and_sort(level_choice, word_pool)
     find_random_word(word_pool)
-    find_and_replace(guess_count, random_word, guessed_letter)
+    print "> "
+    guessed_letter = gets.chomp.downcase
+    find_and_replace(guess_count, guessed_letter, random_word)
   else
-    puts "You can't do that."
+    puts "failed if"
   end
+
+
   #letter and submission phase and functions
   #loop to play again
 
