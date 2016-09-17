02f1c9db5c64500df07f752a326f7219bfde53db
Brandon Fitch <fitchbj@gmail.com>
2016-09-17 13:30:22 -0400

    Getting Find and replace to work second try.  USE THIS AS A BACKUP

--- mystery_word.rb
+++ mystery_word.rb
@@ -12,10 +12,12 @@ def length_and_sort(level_choice, word_pool)
 end
 
 def find_and_replace(guess_count, guessed_letter, random_word)
-  loop do |letter|
+  1.times do
     if random_word.include?(guessed_letter)
-      index_point = random_word.index
-      random_word.replace("!")
+      index_point = random_word.index(guessed_letter)
+      puts "#{index_point}"
+      random_word.delete_at(index_point)
+      print "#{random_word}"
     else
       random_word.include?(guessed_letter)
       puts "You've guessed wrong!"
@@ -26,18 +28,20 @@ end
 
 # random_word = ["a", "a", "b"]
 
-def find_random_word(right_words)
-  random_word = right_words[rand(right_words.length)]
-  puts "#{random_word}"
-  random_word_letter_count = random_word.scan(/./) # string.each_char makes this easy
-  playfield = random_word_letter_count.map{ |letter| letter = "_" }
-  word_as_blanks = playfield.join(" ")
-  puts "#{word_as_blanks}"
-end
+# def find_random_word(right_words)
+#   random_word = right_words[rand(right_words.length)]
+#   puts "#{random_word}"
+#   random_word_letter_count = random_word.scan(/./)
+#   random_word = random_word_letter_count# string.each_char makes this easy
+#   playfield = random_word_letter_count.map{ |letter| letter = "_" }
+#   word_as_blanks = playfield.join(" ")
+#   print random_word
+#   puts "#{word_as_blanks}"
+# end
 
 def main()
-  random_word = []
   guess_count = 0
+
   guessed_letter = ""
   word_pool = File.new("/usr/share/dict/words", 'r')
   word_pool = word_pool.readlines.map(&:chomp)
@@ -45,7 +49,19 @@ def main()
   print "> "
   level_choice = gets.chomp.downcase
   if length_and_sort(level_choice, word_pool)
-    find_random_word(word_pool)
+
+    random_word = word_pool[rand(word_pool.length)]
+
+    puts "#{random_word}"
+    random_word_letter_count = random_word.scan(/./)
+    random_word = random_word_letter_count# string.each_char makes this easy
+    playfield = random_word_letter_count.map{ |letter| letter = "_" }
+    word_as_blanks = playfield.join(" ")
+
+    print random_word
+
+    puts "#{word_as_blanks}"
+
     print "> "
     guessed_letter = gets.chomp.downcase
     find_and_replace(guess_count, guessed_letter, random_word)
@@ -53,7 +69,7 @@ def main()
     puts "failed if"
   end
 
-
+# random word needs to equal the random word array AFTER it selects it.
   #letter and submission phase and functions
   #loop to play again
 
