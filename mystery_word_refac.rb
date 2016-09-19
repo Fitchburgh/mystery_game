2536e2088acf5ef1063f2bc137901551e7929721
Brandon Fitch <fitchbj@gmail.com>
2016-09-18 20:56:56 -0400

    Working except, no restart after losing on guess count, and won't win if the full word is input one guess at a time. doesnt ask to play again upon win either.

--- mystery_word_refac.rb
+++ mystery_word_refac.rb
@@ -23,7 +23,7 @@ def get_random_word(right_length_words)
 end
 
 def word_to_array(random_word)
-  random_word_letter_array = random_word.scan(/./) # scans for one letter, returns each letter as an item in array
+  random_word_letter_array = random_word.scan(/./)
   return random_word_letter_array
 end
 
@@ -42,14 +42,10 @@ def correct_word_guess(guess, random_word_string)
   end
 end
 
-def get_index_of_guess(letter_array, guess, random_word_string, guess_count)
+def get_index_of_guess(letter_array, guess, random_word_string)
   if guess == random_word_string
      correct_word_guess(guess, random_word_string)
      exit
-  elsif random_word_string.include?(guess) == false
-    guess_count += 1
-    puts "Wrong answer."
-    puts "Guess ##{guess_count} of 8. Try again."
   elsif letter_array.include? guess
     index_point = letter_array.index(guess)
     return index_point
@@ -73,12 +69,10 @@ def add_guess_to_underscores(all_guesses, underscore_array, index_point, guess)
 end
 
 def find_and_replace(letter_array, guess, index_point, all_guesses, underscore_array, guess_count, random_word_string)
-  puts "#{guess_count}"
-  get_index_of_guess(letter_array, guess, random_word_string, guess_count)
-  puts "#{guess_count}"
+  get_index_of_guess(letter_array, guess, random_word_string)
   letter_array.each do |letter|
     if letter_array.include?(guess)
-      index_point = get_index_of_guess(letter_array, guess, random_word_string, guess_count)
+      index_point = get_index_of_guess(letter_array, guess, random_word_string)
       get_index_replace_letter(letter_array, index_point)
       underscore_array.each do |underscore|
         add_guess_to_underscores(all_guesses, underscore_array, index_point, guess)
@@ -89,27 +83,34 @@ end
 
 def same_letter_check(all_guesses, guess)
   if dupes_letter = all_guesses.include?(guess)
-    # guess_count -= 1
     return dupes_letter
   else
     return false
   end
 end
 
-def everything(random_word_letter_array, guess, index_of_guess, all_guesses, word_as_underscores, guess_count, random_word_string, joined_word)
+def everything(random_word_letter_array, guess, index_of_guess, all_guesses, word_as_underscores, guess_count, random_word_string, joined_word, guess_counter_other)
   loop do
     if guess_count < 8
-      # correct_word_guess(guess, random_word_string) == false
       puts "What's your letter guess or entire word"
       guess = gets.chomp.downcase
-      puts "#{guess_count}"
-      find_and_replace_var = find_and_replace(random_word_letter_array, guess, index_of_guess, all_guesses, word_as_underscores, guess_count, random_word_string)
-      p word_as_underscores.join(" ")
-      if same_letter_check(all_guesses, guess)
-        puts "You can't do that"
+      if random_word_string.include?(guess) == false
+        if same_letter_check(all_guesses, guess) == true
+        else
+          guess_count += 1
+          puts "Wrong answer. You're on #{guess_count} of 8"
+        end
       end
-      all_guesses << guess
+    else
+      puts "Game over, too many guesses. The word was #{random_word_string}"
+      exit
     end
+    find_and_replace_var = find_and_replace(random_word_letter_array, guess, index_of_guess, all_guesses, word_as_underscores, guess_count, random_word_string)
+    p word_as_underscores.join(" ")
+    if same_letter_check(all_guesses, guess)
+      puts "You can't guess the same letter again! No harm, no foul."
+    end
+    all_guesses << guess
   end
 end
 
@@ -128,6 +129,7 @@ def main()
   }
   all_guesses = []
   guess_count = 0
+  guess_counter_other = 0
   word_pool = File.new("/usr/share/dict/words", 'r')
   word_pool = word_pool.readlines.map(&:chomp)
   choice = get_level_choice
@@ -136,24 +138,12 @@ def main()
   random_word = get_random_word(right_length_words)
   puts "#{random_word}"
   random_word_letter_array = word_to_array(random_word)
-
   word_as_underscores = word_to_underscores(random_word_letter_array)
-
   joined_word = word_as_underscores.join(" ")
-  puts "#{random_word_letter_array}"
-  # puts "What's your letter guess or entire word"
-  # guess = gets.chomp.downcase
-  # correct_word_guess(guess, random_word)
-
-  # put fxn here that does these 3 functions:
   puts "#{joined_word}"
   loop do
-    do_everything = everything(random_word_letter_array, guess, index_of_guess, all_guesses, word_as_underscores, guess_count, random_word, joined_word)
+    do_everything = everything(random_word_letter_array, guess, index_of_guess, all_guesses, word_as_underscores, guess_count, random_word, joined_word, guess_counter_other)
   end
-
-  # end fxn
-  puts "#{all_guesses}"
-
 end
 
 if __FILE__ == $PROGRAM_NAME
